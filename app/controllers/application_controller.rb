fce2b1b20e67ec3ae243be6797aab1b1288d11b1
fiowang <fiona@chicagobooth.edu>
2022-10-20 12:53:16 +0000

    in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,6 +26,9 @@ class ApplicationController < ActionController::Base
 
   def calculate_square_root
     
+    @num = params.fetch("elephant").to_f
+    @square_of_num = @num ** 0.5
+
     render({ :template => "calculation_templates/square_root_results.html.erb" }) 
 
   end
@@ -40,6 +43,9 @@ class ApplicationController < ActionController::Base
 
   def calculate_payment
     
+    @num = params.fetch("elephant").to_f
+    @square_of_num = @num ** 2
+
     render({ :template => "calculation_templates/payment_results.html.erb" }) 
 
   end
@@ -54,6 +60,9 @@ class ApplicationController < ActionController::Base
 
   def calculate_random
     
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+
     render({ :template => "calculation_templates/random_results.html.erb" }) 
 
   end
